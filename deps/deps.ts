1b477fc7bdd348a38dc98b8b406655662f2f699d
yttiiz <dominique.talis@proton.me>
2025-10-03 22:17:35 -0400

    chore(deno): update imports to use short JSR aliases

--- deps/deps.ts
+++ deps/deps.ts
@@ -1,7 +1,7 @@
-import * as oak from "jsr:@oak";
-import { oakCors } from "jsr:cors";
-import { load } from "jsr:dotenv";
-import { DateFormatter } from "jsr:utils";
-import * as nodemailer from "nodemailer";
+import * as oak from "@jsr:oak";
+import { oakCors } from "@jsr:cors";
+import { load } from "@jsr:dotenv";
+import { DateFormatter } from "@jsr:utils";
+import * as nodemailer from "@nodemailer";
 
 export { DateFormatter, load, nodemailer, oak, oakCors };
